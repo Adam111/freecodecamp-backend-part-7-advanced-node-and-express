0e2ca245cd0c954d82d3c7586c33449dc361e9b4
Glitch (arrow-tune) <none>
2018-12-13 19:33:45 +0000

    💺👳 Updated with Glitch

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const express    = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const mongo      = require('mongodb').MongoClient;
+
+const routes = require('./routes.js');
+const auth = require('./auth.js');
 
 const app = express();
 
@@ -11,11 +15,27 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.set('view engine', 'pug');
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+mongo.connect(process.env.DATABASE, { useNewUrlParser: true }, (err, db) => {
+  if (err) {
+    console.log('Database error: ' + err);
+  } else {
+    console.log('Successful database connection');
+    
+    auth(app, db);
+    routes(app, db);
+    
+    // app.listen
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  }
 });
+
+// Enables to pass the challenge called "Advanced Node and Express - Registration of New Users"
+if (process.env.ENABLE_DELAYS) {
+  app.use((req, res, next) => {
+    return setTimeout(() => next(), 500);
+  });
+}
